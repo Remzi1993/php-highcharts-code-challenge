c8b9b1de28b38c374f474d291896cbc4c5719a95
Remzi Cavdar <ict@remzi.info>
2019-04-23 07:51:22 +0200

    Example code

--- example/index.php
+++ example/index.php
@@ -1,20 +1,94 @@
 <?php
-function process_csv($file) {
+$data=[
+    ["Product","Price","Quantity","Date"],
+    ["Teddybear",3789.33,2,"2018-08-14"],
+    ["Teddybear",2702.17,1,"2018-02-17"],
+    ["Table",2932.29,5,"2018-10-09"],
+    ["Lamp",671.70,3,"2018-12-11"],
+    ["Teddybear",481.21,3,"2018-03-24"],
+    ["Table",45.52,3,"2018-05-14"],
+    ["Chair",2960.20,2,"2018-06-20"]
+];
 
-    $file = fopen($file, 'r');
-    $data = array();
 
-    while (!feof($file)) {
-        $data[] = fgetcsv($file,null,';');
-    }
-
-    fclose($file);
-    return $data;
+function WhereFromColum($data,$colum,$equal){
+    $ret=[];
+    array_push($ret,$data[0]);  // *
+    $colum = is_numeric($colum)?$colum:array_search($colum,$data[0]);
+    for($i=1/* 0 if you remove the header to data*/;$i!=count($data);$i++)
+        if ($data[$i][$colum]==$equal)
+            array_push($ret,$data[$i]);
+    return $ret;
 }
 
-// Set path to CSV file
-$csv_file = DIR_PATH . '/data/sales2.csv';
-$csv = process_csv($csv_file);
+function CountFromColum($data,$colum){
+    $ret=0;
+    $colum = is_numeric($colum)?$colum:array_search($colum,$data[0]);
+    for($i=1/* 0 if you remove the header to data*/;$i!=count($data);$i++)
+        $ret+=$data[$i][$colum];
+    return $ret;
+}
+
+function WhereFromColumData($data,$colum,$min,$max){
+    $contractDateBegin = new DateTime($min);
+    $contractDateEnd  = new DateTime($max);
+    $ret=[];
+    array_push($ret,$data[0]);  // *
+    $colum = is_numeric($colum)?$colum:array_search($colum,$data[0]);
+    for($i=1/* 0 if you remove the header to data*/;$i!=count($data);$i++){
+        $paymentDate = new DateTime($data[$i][$colum]);
+        if ($paymentDate->getTimestamp() > $contractDateBegin->getTimestamp() &&
+            $paymentDate->getTimestamp() < $contractDateEnd->getTimestamp())
+            array_push($ret,$data[$i]);
+    }
+    return $ret;
+}
+
+
+var_dump(WhereFromColum($data,"Product","Teddybear")); // get all Teddybear's Product
+var_dump(CountFromColum($data,"Quantity")); // get all Quantity
+var_dump(CountFromColum(WhereFromColum($data,"Product","Teddybear"),"Quantity"));  // get all Quantity of Teddybear's Product
+var_dump(WhereFromColumData($data,"Date","2018-08-01","2018-12-01")); // get all between 2018-08-01 and 2018-12-01
+var_dump(WhereFromColumData(WhereFromColum($data,"Product","Teddybear"),"Date","2018-08-01","2018-12-01")); // get all Teddybear's Product between 2018-08-01 and 2018-12-01
+exit;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function process_csv($file) {
+//
+//     $file = fopen($file, 'r');
+//     $data = array();
+//
+//     while (!feof($file)) {
+//         $data[] = fgetcsv($file,null,';');
+//     }
+//
+//     fclose($file);
+//     return $data;
+// }
+//
+// // Set path to CSV file
+// $csv_file = DIR_PATH . '/data/sales2.csv';
+// $csv = process_csv($csv_file);
 
 
 
@@ -36,9 +110,9 @@ $csv = process_csv($csv_file);
 
 
 
-echo '<pre>';
-print_r($csv);
-echo '</pre>';
+// echo '<pre>';
+// print_r($csv);
+// echo '</pre>';
 
 
 
@@ -49,7 +123,7 @@ echo '</pre>';
 // echo '<pre>';
 // print_r($json);
 // echo '</pre>';
-exit;
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
